9856858281ee0cd1e257efb514efcf894ca2a554
sudarshanbhatt <ssbhatt1991@gmail.com>
2021-10-22 16:10:38 +0530

    Update greeting text

--- app/src/main/java/sud/bhatt/exampleforversoning/MainActivity.kt
+++ app/src/main/java/sud/bhatt/exampleforversoning/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) 
+        // add click listener to the button
+        button.setOnClicklistener();
     }
 }
